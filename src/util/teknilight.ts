35e16207bf95f4c4289293d2b848b98dcf2ceb57
Adriaan Knapen <hi@aknapen.nl>
2020-12-06 09:55:44 +0100

    refactor: adds additional comments to setBrightness

--- src/util/teknilight.ts
+++ src/util/teknilight.ts
@@ -1,4 +1,4 @@
-import { connect, connectAsync, IClientPublishOptions } from 'async-mqtt'
+import { connectAsync, IClientPublishOptions } from 'async-mqtt'
 
 const ENVIRONMENT = "prod"
 
@@ -8,6 +8,7 @@ const MQTT_CHANNEL_PREFIX = `tek/${ENVIRONMENT}/light/1`
 const asyncClient = () => connectAsync("wss://mqtt.flespi.io:443", { username: "tfDgEAF5LmNkImKnaz2eTTjjs6NPWgMglfkPENKdZmnvyXJxlBB2kL9DFMgsTTw2"})
 
 export const setBrightness = async (brightness: number): Promise<{ success: boolean }> => {
+  // Input validation
   if (brightness < 0) {
     console.error(`Brightness cannot be lower than 0%, received ${brightness}%`)
     return { success: false }
@@ -21,8 +22,10 @@ export const setBrightness = async (brightness: number): Promise<{ success: bool
     return { success: false }
   }
 
+  // Show the user that we are working on it
   console.error(`💡 Setting brightness to ${brightness}%`) 
 
+  // Attempt to update the brightness
   try {
     const client = await asyncClient()
     client.publish(`${MQTT_CHANNEL_PREFIX}/brightness`, JSON.stringify({ brightness: brightness / 100 }), MQTT_OPTIONS)
